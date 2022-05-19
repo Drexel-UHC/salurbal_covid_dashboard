b607618dcf05af91df8a10bd2f63393bcfac6c73
rl627 <rl627@drexel.edu>
2022-05-19 09:38:26 -0400

    Aded Qc for trends new vs back-calculated cumulative from byAge

--- Data/manuscripts/serena/5_by-age-qc.R
+++ Data/manuscripts/serena/5_by-age-qc.R
@@ -2,7 +2,7 @@
 #' numebrs will be slightly  lower beacause the byAge dataset only keeps rows where age is between 0-84.
 #' 
 
-{## 0. Setup
+{## 0. Setup -----
   library(tidyverse)
   library(data.table)
   library(plotly)
@@ -10,7 +10,7 @@
 }
 
 
-{## 1. Check cumulative numbers
+{## 1. Check cumulative numbers ----
   ## Original data
   df_cum_old = fread("processed-data/salurbal_AR_covid 5-18-22/Aggregated/SALURBAL_AR_COVID19_cumulative_cases_death.csv")
   
@@ -39,7 +39,7 @@
   qc = df_merge%>% 
     ggplot(aes(value_old, value_new, label = salid))+
     geom_point()+
-    facet_grid(vars(level), vars(name), scales = 'fixed')+
+    facet_grid(vars(level), vars(name), scales = 'free')+
     geom_abline(slope = 1, intercept = 0)
   
   ggplotly(qc)
@@ -48,7 +48,7 @@
 }
 
 
-{## 1. Check Trends numbers
+{## 2. Check Trends numbers ----
   ## Original data
   df_trends_old = fread("processed-data/salurbal_AR_covid 5-18-22/Aggregated/SALUBRAL_AR_COVID19_trends_cases_death.csv") 
   min(df_trends_old$date)
@@ -76,24 +76,25 @@
     pivot_longer(cols = -c(salid, date, level), values_to  = 'value_old') %>% 
     left_join(df_trends_new) %>% 
     filter(date<min( max(df_trends_old$date),  max(df_trends_new$date))) %>% 
-    filter(name%in%c('cases_raw','deaths_raw'))
-  
-  df_merge2
-  df_merge2 %>% 
-    pivot_longer(values_from=c(value_old, value_new))
+    filter(name%in%c('cases_raw','deaths_raw')) %>% 
+    rename(metric = name) %>%  
+    pivot_longer(cols=c(value_old, value_new))
 
+  df_merge2 %>% 
+    filter(salid == "101112") %>% 
+    ggplot(aes(x=date, y = value,  color = name, group = salid))+
+    geom_line()+
+    facet_wrap(~metric, scales = 'free')
   
-  filter(name%in%c("cases","deaths")) %>% 
-    
-    qc2 = df_merge2 %>% 
-    filter(level == "L1") %>% 
-    ggplot(aes(x=date, y = value_, value_new, label = salid))+
-    geom_point()+
-    facet_wrap(~name, scales = 'free')+
-    geom_abline(slope = 1, intercept = 0)
-  qc2
-  
+  df_merge2 %>% 
+    filter(salid == "101105") %>% 
+    ggplot(aes(x=date, y = value,  color = name, group = salid))+
+    geom_line()+
+    facet_wrap(~metric, scales = 'free')
   ggplotly(qc)
+}
+
+{ ## 3. Check pop denoms ----
   
   
 }
